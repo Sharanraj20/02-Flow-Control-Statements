b16b4859e2bdd1da3cfce2863376fee85709b0e9
Sharan Raj G <60322880+Sharanraj20@users.noreply.github.com>
2020-06-05 22:03:34 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,19 @@
+  
+public class prog13 {
+
+	public static void main(String[] args) {
+		int flag;
+		for(int x=10;x<99;x++) {
+			flag=0;
+			for(int i=2;i<=Math.sqrt(x);i++) {
+				if(x%i==0) {
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0) {
+				System.out.println(x);
+			}
+		}
+	}
+}
